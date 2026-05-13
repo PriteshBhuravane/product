4a0d1e801eba9050e93a628819014c86cd17566a
Pritesh <bhuravanepritesh@gmail.com>
2026-05-13 15:50:49 +0530

    new changes

--- app/Http/Controllers/UserLogController.php
+++ app/Http/Controllers/UserLogController.php
@@ -10,6 +10,6 @@ class UserLogController extends Controller
     public function index()
     {
         $logs = UserLog::orderByDesc('created_at')->limit(100)->get();
-        return response()->json($logs);
+        return view('admin.admin-logs', ['logs' => $logs]);
     }
 }
